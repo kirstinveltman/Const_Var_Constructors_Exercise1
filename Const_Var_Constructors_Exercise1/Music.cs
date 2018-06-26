d9d2992fbf6490a5a34ad930dd755982eb25d144
Kirstin Veltman <kirstin@veltman.co>
2018-06-26 14:40:49 -0700

    Add project files.

---
+++ Const_Var_Constructors_Exercise1/Music.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Const_Var_Constructors_Exercise1
+{
+    class Music
+    {
+        public Music(string name) : this(name, 1994)
+        {
+        }
+
+        public Music(string song, int yearMade)
+        {
+            Song = song;
+            YearMade = yearMade;
+            Console.WriteLine("This song, " + song + " was created in the year " + yearMade + ".");
+        }
+
+        public string Song { get; set; }
+        public int YearMade { get; set; }
+
+    }
+}
---
+++ Const_Var_Constructors_Exercise1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Const_Var_Constructors_Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string artistName = "The Beatles";
+            Console.WriteLine("One of my favorite bands is {0}.", artistName);
+
+            string song = "Let It Be";
+            int yearMade = 1970;
+            var newMusic = new Music(song, yearMade);
+
+            Console.ReadLine();
+
+        }
+    }
+}
